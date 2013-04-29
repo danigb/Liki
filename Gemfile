a1ec8135ad8cceb20bb006b7ac55cac7b3066575
danigb <danigb@gmail.com>
2013-04-29 04:04:45 +0200

    Font awesome: icons

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jquery-turbolinks'
 gem 'jbuilder', '~> 1.0.1'
+gem 'font-awesome-sass-rails'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,9 @@ module ApplicationHelper
     content_for(:title) { title }
     content_tag :h1, title
   end
+
+  def icon(icon, label = nil)
+    label = label.present? ? h("#{label}") : ''
+    raw("<i class='icon-#{icon}'></i>&nbsp;#{label}")
+  end
 end
