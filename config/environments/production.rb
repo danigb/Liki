24052f89876fcdd2459ecacfe6e49c461995470c
danigb <danigb@gmail.com>
2013-05-24 13:37:48 +0200

    smtp bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Liki::Application.configure do
   config.action_mailer.smtp_settings = {
     address:             "smtp.mandrillapp.com",
     port:                587,
-    user_name:           'danigb@gmail.com'
+    user_name:           'danigb@gmail.com',
     password:            'E1qXXgoRpHsJAkipqHpJhA',
     authentication:      'plain',
     enable_starttls_auto: true  }
