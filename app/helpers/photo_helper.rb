457a744e7019c23647752ef4826832abb02536a1
danigb <danigb@gmail.com>
2013-06-17 11:48:51 +0200

    Remove photo styles

--- app/helpers/photo_helper.rb
+++ app/helpers/photo_helper.rb
@@ -4,4 +4,9 @@ module PhotoHelper
     current_space.photos_node_id == node.id
   end
 
+  def photo_nodes(photo)
+    raw(photo.nodes.where("nodes.id <> ?", current_space.photos_node_id).
+      map {|n| link_to(arrow(n.title), n)}.join(', '))
+  end
+
 end
